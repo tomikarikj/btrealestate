2094ca41347d5a291bf926c01dd1adc5f5640172
Tomi Karikj <karikjtomi@gmail.com>
2019-10-07 21:20:34 +0200

    Added logic for getting data from the database

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import Listing
+from realtors.models import Realtor
+
 
 def index(request):
-    return render(request, 'pages/index.html')
+    # Get three listings ordered by the publish date
+    listings = Listing.objects.order_by(
+        '-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings': listings
+    }
+
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    # Get all the realtors ordered by their hire date
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    # Get the MVP
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+
+    return render(request, 'pages/about.html', context)
