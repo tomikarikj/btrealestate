4c7906f25e8e5e676866a2e9de05fe1f6bcc7121
Tomi Karikj <karikjtomi@gmail.com>
2019-10-09 18:18:10 +0200

    Created the accounts app and the templates for it

---
+++ accounts/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+
+def register(request):
+    if request.method == 'POST':
+        messages.error(request, 'Testing the error alert')
+        return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        # Log in the user
+        return
+    else:
+        return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    return redirect('index')
+
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
