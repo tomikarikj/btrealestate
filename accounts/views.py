4a4bd1a2cbe93529e31bd7b64c8f5d215da0176e
Tomi Karikj <karikjtomi@gmail.com>
2019-10-09 20:47:21 +0200

    The logic for registration, logging in and out

--- accounts/views.py
+++ accounts/views.py
@@ -1,25 +1,70 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'Testing the error alert')
-        return redirect('register')
+        # Get the form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Check if the passwords match
+        if password == password2:
+            # Check if the username already exists
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken.')
+                return redirect('register')
+            else:
+                # Check if there is already an account with the email from the form
+                if User.objects.filter(email=email).exists():
+                    messages.error(
+                        request, 'There is already an account for that email.')
+                    return redirect('register')
+                else:
+                    # Everything has passed
+                    user = User.objects.create_user(
+                        username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+                    # Redirect the user to the login page after register
+                    user.save()
+                    messages.success(
+                        request, 'The registration was successful. You can now log in.')
+                    return redirect('login')
+        else:
+            messages.error(
+                request, 'Passwords do not match. Please try again.')
+            return redirect('login')
     else:
         return render(request, 'accounts/register.html')
 
 
 def login(request):
     if request.method == 'POST':
-        # Log in the user
-        return
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now successfully logged in!')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid credentials. Please try again.')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
 
 def logout(request):
-    return redirect('index')
+    if request.method == 'POST':
+        auth.logout(request)
+        # messages.success(request, 'You are now logged out.')
+        return redirect('index')
 
 
 def dashboard(request):
